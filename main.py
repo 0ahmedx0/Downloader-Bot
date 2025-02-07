e631cb1ca7fd8bd8d839a156f78cf0f02d376811
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-02-07 04:35:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,10 +43,6 @@ async def send_analytics(user_id, chat_type, action_name):
             }
         }],
     }
-    async with httpx.AsyncClient() as client:
-        await client.post(
-            f'https://www.google-analytics.com/mp/collect?measurement_id={MEASUREMENT_ID}&api_secret={API_SECRET}',
-            json=params)
 
 
 async def main():
