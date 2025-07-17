d965f98b2954cd90f1b3d88f6fb89a63bc4fcff6
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-07-17 07:35:43 +0300

    Update albumchanal.py

--- albumchanal.py
+++ albumchanal.py
@@ -30,8 +30,7 @@ from telegram.constants import ParseMode
 
 # إعداد التسجيل
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - "%(message)s", level=logging.INFO
-)
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # الحالات للمحادثة
