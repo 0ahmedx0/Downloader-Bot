ee12ef7a46d06607c81f4b66f9595dda05650312
0ahmedx0 <144847462+0ahmedx0@users.noreply.github.com>
2025-09-02 00:57:43 +0300

    Update twitter.py

--- handlers/twitter.py
+++ handlers/twitter.py
@@ -21,6 +21,7 @@ album_accumulator = {}  # الصيغة: { chat_id: {"image": [(file_path, type, 
 chat_queues = {}        # قاموس لحفظ قوائم الانتظار لكل دردشة
 chat_workers = {}       # قاموس لحفظ مهام المعالجة لكل دردشة
 
+
 def extract_tweet_ids(text):
     """Extract tweet IDs from message text."""
     unshortened_links = ''
@@ -30,8 +31,12 @@ def extract_tweet_ids(text):
             unshortened_links += '\n' + unshortened_link
         except:
             pass
-    tweet_ids = re.findall(r"(?:twitter|x)\.com/.{1,15}/(?:web|status(?:es)?)/([0-9]{1,20})", text + unshortened_links)
+    tweet_ids = re.findall(
+        r"(?:twitter|x)\.com/.{1,15}/(?:web|status(?:es)?)/([0-9]{1,20})",
+        text + unshortened_links,
+    )
     return list(dict.fromkeys(tweet_ids)) if tweet_ids else None
+
 
 def scrape_media(tweet_id):
     r = requests.get(f'https://api.vxtwitter.com/Twitter/status/{tweet_id}')
@@ -43,6 +48,7 @@ def scrape_media(tweet_id):
             raise Exception(f'API returned error: {html.unescape(match.group(1))}')
         raise
 
+
 async def download_media(media_url, file_path):
     response = requests.get(media_url, stream=True)
     response.raise_for_status()
@@ -50,18 +56,22 @@ async def download_media(media_url, file_path):
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
 
+
 async def reply_media(message, tweet_id, tweet_media, bot_url, business_id):
     """معالجة الوسائط مع استخدام قائمة الانتظار وتخزين الصور والفيديوهات منفصلين"""
     await send_analytics(user_id=message.from_user.id, chat_type=message.chat.type, action_name="twitter")
     tweet_dir = f"{OUTPUT_DIR}/{tweet_id}"
     post_caption = tweet_media["text"]
     user_captions = await db.get_user_captions(message.from_user.id)
+
     if not os.path.exists(tweet_dir):
         os.makedirs(tweet_dir)
+
     key = message.chat.id
     # تهيئة القاموس الخاص بأنواع الوسائط إن لم يكن موجوداً
     if key not in album_accumulator:
         album_accumulator[key] = {"image": [], "video": []}
+
     try:
         # تنزيل الوسائط وتخزينها بناءً على النوع
         for media in tweet_media['media_extended']:
@@ -69,6 +79,7 @@ async def reply_media(message, tweet_id, tweet_media, bot_url, business_id):
             media_type = media['type']
             file_name = os.path.join(tweet_dir, os.path.basename(urlsplit(media_url).path))
             await download_media(media_url, file_name)
+
             if media_type == 'image':
                 album_accumulator[key]["image"].append((file_name, media_type, tweet_dir))
             elif media_type in ['video', 'gif']:
@@ -80,6 +91,7 @@ async def reply_media(message, tweet_id, tweet_media, bot_url, business_id):
             media_group = MediaGroupBuilder(caption=bm.captions(user_captions, post_caption, bot_url))
             for file_path, media_type, _ in album_to_send:
                 media_group.add_photo(media=FSInputFile(file_path))
+
             while True:
                 try:
                     sent_messages = await message.answer_media_group(media_group.build())
@@ -88,25 +100,30 @@ async def reply_media(message, tweet_id, tweet_media, bot_url, business_id):
                     print(f"FloodWait: الانتظار لمدة {e.retry_after} ثانية قبل إعادة المحاولة")
                 except TelegramRetryAfter as e:
                     print(f"TelegramRetryAfter: الانتظار لمدة {e.retry_after} ثانية قبل إعادة المحاولة")
-                    await asyncio.sleep(e.retry_after)
+                await asyncio.sleep(e.retry_after)
+
             # إزالة الصور المرسلة من القائمة
             album_accumulator[key]["image"] = album_accumulator[key]["image"][5:]
+
             # حذف الملفات المرسلة من القرص
             for file_path, _, dir_path in album_to_send:
                 if os.path.exists(file_path):
                     os.remove(file_path)
                 if os.path.exists(dir_path) and not os.listdir(dir_path):
                     os.rmdir(dir_path)
+
             # إضافة تأخير بعد إرسال الألبوم لتجنب الحظر
             await asyncio.sleep(5)
 
         # إرسال الفيديوهات إذا كانت هناك 5 فيديوهات أو أكثر
         if len(album_accumulator[key]["video"]) >= 5:
             album_to_send = album_accumulator[key]["video"][:5]
+
             # إضافة وصف "فيديو" للألبوم
             media_group = MediaGroupBuilder(caption="فيديو")
             for file_path, media_type, _ in album_to_send:
                 media_group.add_video(media=FSInputFile(file_path))
+
             while True:
                 try:
                     sent_messages = await message.answer_media_group(media_group.build())
@@ -115,21 +132,26 @@ async def reply_media(message, tweet_id, tweet_media, bot_url, business_id):
                     print(f"FloodWait: الانتظار لمدة {e.retry_after} ثانية قبل إعادة المحاولة")
                 except TelegramRetryAfter as e:
                     print(f"TelegramRetryAfter: الانتظار لمدة {e.retry_after} ثانية قبل إعادة المحاولة")
-                    await asyncio.sleep(e.retry_after)
+                await asyncio.sleep(e.retry_after)
+
             album_accumulator[key]["video"] = album_accumulator[key]["video"][5:]
+
             for file_path, _, dir_path in album_to_send:
                 if os.path.exists(file_path):
                     os.remove(file_path)
                 if os.path.exists(dir_path) and not os.listdir(dir_path):
                     os.rmdir(dir_path)
+
             # إضافة تأخير بعد إرسال الألبوم لتجنب الحظر
             await asyncio.sleep(5)
+
     except Exception as e:
         print(e)
         if business_id is None:
             react = types.ReactionTypeEmoji(emoji="👎")
             await message.react([react])
-        await message.reply("Something went wrong :(\nPlease try again later.")
+            await message.reply("Something went wrong :(\nPlease try again later.")
+
 
 async def process_chat_queue(chat_id):
     """معالجة الرسائل في قائمة الانتظار بالتتابع"""
@@ -143,16 +165,20 @@ async def process_chat_queue(chat_id):
             if business_id is None:
                 react = types.ReactionTypeEmoji(emoji="👨‍💻")
                 await message.react([react])
+
             bot_url = f"t.me/{(await bot.get_me()).username}"
             tweet_ids = extract_tweet_ids(message.text)
+
             if tweet_ids:
                 if business_id is None:
                     await bot.send_chat_action(message.chat.id, "typing")
+
                 for tweet_id in tweet_ids:
                     media = scrape_media(tweet_id)
                     await reply_media(message, tweet_id, media, bot_url, business_id)
                     # تأخير زمني بين معالجة كل تغريدة
                     await asyncio.sleep(3)
+
                 await asyncio.sleep(2)
                 try:
                     await message.delete()
@@ -165,6 +191,7 @@ async def process_chat_queue(chat_id):
                 await message.answer("No tweet IDs found.")
         finally:
             chat_queues[chat_id].task_done()
+
 
 @router.message(F.text.regexp(r"(https?://(www\.)?(twitter|x)\.com/\S+|https?://t\.co/\S+)"))
 @router.business_message(F.text.regexp(r"(https?://(www\.)?(twitter|x)\.com/\S+|https?://t\.co/\S+)"))
